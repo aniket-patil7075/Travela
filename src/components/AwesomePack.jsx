fff3b663bbe693b4631b502f1be597f32ec12103
aniket-patil7075 <akp7075@gmail.com>
2024-09-27 17:18:58 +0530

    Upto Awesome Package is done

--- src/components/AwesomePack.jsx
+++ src/components/AwesomePack.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
-import { FaArrowLeft } from "react-icons/fa";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import { FaStar } from "react-icons/fa";
 
 function AwesomePack() {
   const [activeButton, setActiveButton] = useState('');
@@ -14,40 +11,55 @@ function AwesomePack() {
 
   const cards = [
     {
-      title: "Card 1",
-      text: "This is card 1 with supporting text.",
-      updated: "Last updated 3 mins ago",
+      place:"Venice-Italy",
+      days:"3 Days",
+      person:"2 Person",
+      src: "/packages-1.jpg",
+      title: "Thailand Trip",
+      text1: "HOTEL DEALS",
+      text2: <><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></>,
+      text3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempore nam, architecto doloremque",
+      updated1: "Read More",
+      updated2: "Book Now",
     },
     {
-      title: "Card 2",
-      text: "This is card 2 with supporting text.",
-      updated: "Last updated 5 mins ago",
+      src: "/packages-2.jpg",
+      title: "Venice - Italy",
+      text1: "HOTEL DEALS",
+      text2: <><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></>,
+      text3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempore nam, architecto doloremque",
+      updated1: "Read More",
+      updated2: "Book Now",
     },
     {
-      title: "Card 3",
-      text: "This is card 3 with supporting text.",
-      updated: "Last updated 10 mins ago",
+      src: "packages-3.jpg",
+      title: "The New California",
+      text1: "HOTEL DEALS",
+      text2: <><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></>,
+      text3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempore nam, architecto doloremque",
+      updated1: "Read More",
+      updated2: "Book Now",
     },
     {
-      title: "Card 4",
-      text: "This is card 4 with supporting text.",
-      updated: "Last updated 15 mins ago",
-    },
-    {
-      title: "Card 5",
-      text: "This is card 5 with supporting text.",
-      updated: "Last updated 20 mins ago",
-    },
+      src: "packages-4.jpg",
+      title: "Discover Japan",
+      text1: "HOTEL DEALS",
+      text2: <><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></>,
+      text3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempore nam, architecto doloremque",
+      updated1: "Read More",
+      updated2: "Book Now",
+    }
   ];
 
   const handleButtonClick = (direction) => {
     if (direction === 'LeftArrow' && visibleIndex > 0) {
       setVisibleIndex(visibleIndex - 1);
-    } else if (direction === 'RightArrow' && visibleIndex + 3 < cards.length) {
+    } else if (direction === 'RightArrow' && visibleIndex < cards.length - 3) {
       setVisibleIndex(visibleIndex + 1);
     }
     setActiveButton(direction);
   };
+
   return (
     <div className='' style={{ marginTop: '120px' }}>
       <Container>
@@ -71,26 +83,46 @@ function AwesomePack() {
           </Button>
         </div>
 
-        <div className=''>
-        <div className='d-flex'>
-          {cards.slice(visibleIndex, visibleIndex + 3).map((card, index) => (
-            <Card className='packCard' key={index}>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>{card.title}</Card.Title>
-                <Card.Text>{card.text}</Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">{card.updated}</small>
-              </Card.Footer>
-            </Card>
-          ))}
-        </div>
+        <div className="overflow-hidden">
+          <div
+            className="card-container"
+            style={{
+              display: 'flex',
+              transition: 'transform 0.5s ease-in-out',
+              transform: `translateX(-${visibleIndex * (100 / 3)}%)`,
+            }}
+          >
+            {cards.map((card, index) => (
+              <Card className="packCard mx-3 " key={index} >
+                <div className=' card-img'>
+                  <Card.Img variant="top" className='' src={card.src} />
+                  <div className='explore1 d-flex'>
+                    <p className='px-3'>{card.place} </p>
+                    <p className='px-3'>{card.days} </p>
+                    <p className='px-3'>{card.person} </p>
+                  </div>
+                  <div className='opaci1'></div>
+                </div>
 
+                <Card.Body>
+                  <Card.Title className='my-0'>{card.title}</Card.Title>
+                  <Card.Text className='my-0'>{card.text1}</Card.Text>
+                  <Card.Text className='colorBlue'>{card.text2}</Card.Text>
+                  <Card.Text>{card.text3}</Card.Text>
+                </Card.Body>
+                <Card.Footer className='' style={{ backgroundColor: "#13357B" }}>
+                  <div className='d-flex justify-content-between py-1'>
+                    <small className="fw-bold text-light">{card.updated1}</small>
+                    <small className="fw-bold text-light">{card.updated2}</small>
+                  </div>
+                </Card.Footer>
+              </Card>
+            ))}
+          </div>
         </div>
       </Container>
     </div>
-  )
+  );
 }
 
-export default AwesomePack
+export default AwesomePack;
